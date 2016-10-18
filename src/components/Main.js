34f5b99d01508b503f5b1e7e0e04aac44b79987c
Luis Arce <luis.arce22.a@gmail.com>
2016-10-17 23:34:11 -0400

    Updated Main.js to use the Name component created

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import Name from './my/namespaced/components/NameComponent';
 
 let yeomanImage = require('../images/yeoman.png');
 
@@ -12,6 +13,7 @@ class AppComponent extends React.Component {
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
         <div>This is a example to work with react js </div>
+        <Name />
       </div>
     );
   }
